bb8d63cd5e4dd196832238104be0785a8e05cf0f
Nguyễn Minh Tâm <137066831+tententamy@users.noreply.github.com>
2024-09-25 21:35:55 +0700

    Service and ServiceCategory

--- KVSC.Data/UnitOfWork.cs
+++ KVSC.Data/UnitOfWork.cs
@@ -9,6 +9,8 @@ namespace KVSC.Data
     {
         private FA24_SE1720_231_G5_KVSCContext _context;
         private CustomerRepository _customerRepository;
+        private ServiceRepository _serviceRepository;
+        private ServiceCategoryRepository _categoryRepository;
         private ServiceRequestRepository _serviceRequestRepository;
         public UnitOfWork()
         {
@@ -26,21 +28,38 @@ namespace KVSC.Data
             get { return _serviceRequestRepository ??= new ServiceRequestRepository(_context); }
         }
 
+        public ServiceCategoryRepository serviceCategoryRepository
+        {
+            get
+            {
+                return _categoryRepository ??= new Repository.ServiceCategoryRepository(_context);
+            }
+
+        }
+
+        public ServiceRepository serviceRepository
+        {
+            get
+            {
+                return _serviceRepository ??= new Repository.ServiceRepository(_context);
+            }
+        }
+
         ////TO-DO CODE HERE/////////////////
 
-        #region Set transaction isolation levels
+            #region Set transaction isolation levels
 
-        /*
-        Read Uncommitted: The lowest level of isolation, allows transactions to read uncommitted data from other transactions. This can lead to dirty reads and other issues.
+            /*
+            Read Uncommitted: The lowest level of isolation, allows transactions to read uncommitted data from other transactions. This can lead to dirty reads and other issues.
 
-        Read Committed: Transactions can only read data that has been committed by other transactions. This level avoids dirty reads but can still experience other isolation problems.
+            Read Committed: Transactions can only read data that has been committed by other transactions. This level avoids dirty reads but can still experience other isolation problems.
 
-        Repeatable Read: Transactions can only read data that was committed before their execution, and all reads are repeatable. This prevents dirty reads and non-repeatable reads, but may still experience phantom reads.
+            Repeatable Read: Transactions can only read data that was committed before their execution, and all reads are repeatable. This prevents dirty reads and non-repeatable reads, but may still experience phantom reads.
 
-        Serializable: The highest level of isolation, ensuring that transactions are completely isolated from one another. This can lead to increased lock contention, potentially hurting performance.
+            Serializable: The highest level of isolation, ensuring that transactions are completely isolated from one another. This can lead to increased lock contention, potentially hurting performance.
 
-        Snapshot: This isolation level uses row versioning to avoid locks, providing consistency without impeding concurrency. 
-         */
+            Snapshot: This isolation level uses row versioning to avoid locks, providing consistency without impeding concurrency. 
+             */
 
         public int SaveChangesWithTransaction()
         {
