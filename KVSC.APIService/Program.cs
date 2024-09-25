f492158da5d4a98c8ae48b30534b1aacd33673c9
huynhduc <lengochuynhduc7@gmail.com>
2024-09-25 10:43:46 +0700

    Add project files.

---
+++ KVSC.APIService/Program.cs
@@ -0,0 +1,31 @@
+using KVSC.Data.Models;
+using KVSC.Service.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<FA24_SE1720_231_G5_KVSCContext>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IServiceRequestService, ServiceRequestService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
