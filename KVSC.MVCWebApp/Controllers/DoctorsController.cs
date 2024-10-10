6cad48108dc59e8c65f79079042784a352962074
thanhphongne <thanhphongmainguyen@gmail.com>
2024-10-10 20:02:22 +0700

    doctorv2

---
+++ KVSC.MVCWebApp/Controllers/DoctorsController.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KVSC.Data.Models;
+using KVSC.Common;
+using KVSC.Service.Base;
+using Newtonsoft.Json;
+
+namespace KVSC.MVCWebApp.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private readonly FA24_SE1720_231_G5_KVSCContext _context;
+
+        public DoctorsController(FA24_SE1720_231_G5_KVSCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Doctors
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndPoint + "Doctors"))
+                {
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<List<Doctor>>
+                                (result.Data.ToString());
+
+                            return View(data);
+                        }
+                    }
+                }
+            }
+
+            return View(new List<Doctor>());
+        }
+
+
+        // GET: Doctors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndPoint + $"Doctors/{id}"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<Doctor>(result.Data.ToString());
+                            return View(data);
+                        }
+                    }
+                }
+            }
+
+            return View(new Doctor());
+        }
+
+        // GET: Doctors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DoctorId,FullName,DateOfBirth,PhoneNumber,Email,Address,Specialization,YearsOfExperience,RatingAverage,Status,ProfilePicture")] Doctor doctor)
+        {
+            bool saveStatus = false;
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.PostAsJsonAsync(Const.APIEndPoint + "Doctors/", doctor))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                            if (result != null && result.Status == Const.SUCCESS_CREATE_CODE)
+                            {
+                                var data = JsonConvert.DeserializeObject<Doctor>(result.Data.ToString());
+                                saveStatus = true;
+                            }
+                            else
+                            {
+                                saveStatus = false;
+                            }
+
+                        }
+                    }
+
+
+                }
+
+            }
+            if (saveStatus)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction(nameof(Create));
+            }
+        }
+
+        // GET: Doctors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var doctor = new Doctor();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndPoint + "Doctors/" + id))
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            doctor = JsonConvert.DeserializeObject<Doctor>(result.Data.ToString());
+                        }
+                    }
+            }
+
+            return View(doctor);
+        }
+
+        // POST: Doctors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DoctorId,FullName,DateOfBirth,PhoneNumber,Email,Address,Specialization,YearsOfExperience,RatingAverage,Status,ProfilePicture")] Doctor doctor)
+        {
+            bool saveStatus = false;
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.PostAsJsonAsync(Const.APIEndPoint + "Doctors/", doctor))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                            if (result != null && result.Status == Const.SUCCESS_UPDATE_CODE)
+                            {
+                                var data = JsonConvert.DeserializeObject<Doctor>(result.Data.ToString());
+                                saveStatus = true;
+                            }
+                            else
+                            {
+                                saveStatus = false;
+                            }
+
+                        }
+                    }
+
+
+                }
+
+            }
+            if (saveStatus)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction(nameof(Edit));
+            }
+        }
+
+        // GET: Doctors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var respone = await httpClient.GetAsync(Const.APIEndPoint + "Doctors/" + id))
+                {
+                    if (respone.IsSuccessStatusCode)
+                    {
+                        var content = await respone.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<Doctor>(result.Data.ToString());
+                            return View(data);
+                        }
+
+                    }
+                }
+                return View(new Doctor());
+            }
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            bool deleteStatus = false;
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.DeleteAsync(Const.APIEndPoint + "Doctors/" + id))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                            if (result != null && result.Status == Const.SUCCESS_DELETE_CODE)
+                            {
+                                deleteStatus = true;
+                            }
+                            else
+                            {
+                                deleteStatus = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if (deleteStatus)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction(nameof(Delete));
+            }
+        }
+
+        private bool DoctorExists(int id)
+        {
+            return _context.Doctors.Any(e => e.DoctorId == id);
+        }
+    }
+}
